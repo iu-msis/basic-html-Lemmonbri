2cb49563007addd2c9c473ab1246c7ec4a83c125
Brian Emmons <lemmonbri@gmail.com>
2019-09-15 21:50:11 -0400

    Fixed js file to have blank json structure.

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,20 +1,69 @@
-var waitingApp = new Vue ({
+var waitingApp = new Vue({
   el: '#patientWaitingApp',
   data: {
-    patients: [
-      {
-      }
-    ]
+    patients:{
+          "gender": "",
+          "name": {
+            "title": "",
+            "first": "",
+            "last": ""
+          },
+          "location": {
+            "street": "",
+            "city": "",
+            "state": "",
+            "postcode": "",
+            "coordinates": {
+              "latitude": "",
+              "longitude": ""
+            },
+            "timezone": {
+              "offset": "",
+              "description": ""
+            }
+          },
+          "email": "",
+          "login": {
+            "uuid": "",
+            "username": "",
+            "password": "",
+            "salt": "",
+            "md5": "",
+            "sha1": "",
+            "sha256": ""
+          },
+          "dob": {
+            "date": "",
+            "age": null
+          },
+          "registered": {
+            "date": "",
+            "age": null
+          },
+          "phone": "",
+          "cell": "",
+          "id": {
+            "name": "",
+            "value": ""
+          },
+          "picture": {
+            "large": "",
+            "medium": "",
+            "thumbnail": ""
+          },
+          "nat": ""
+    }
   },
+
   methods: {
     fetchPatients() {
       fetch('https://randomuser.me/api')
       .then(response => response.json())
-      .then(json => waitingApp.patients = json.results[0])
-      ;
+      .then(json => waitingApp.patients = json.results[0]);
+
     }
   },
   created() {
     this.fetchPatients();
   }
-});
+})
