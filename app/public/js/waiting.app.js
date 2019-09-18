766ac889ca45faf2063b2460d42023c37c17722b
Brian Emmons <lemmonbri@gmail.com>
2019-09-18 12:56:37 -0400

    Reconfigured JS files to be unique for each different table/form

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,65 +1,13 @@
-var waitingApp = new Vue({
+var patientWaitingApp = new Vue({
   el: '#patientWaitingApp',
   data: {
-    patients:{
-          "gender": "",
-          "name": {
-            "title": "",
-            "first": "",
-            "last": ""
-          },
-          "location": {
-            "street": "",
-            "city": "",
-            "state": "",
-            "postcode": "",
-            "coordinates": {
-              "latitude": "",
-              "longitude": ""
-            },
-            "timezone": {
-              "offset": "",
-              "description": ""
-            }
-          },
-          "email": "",
-          "login": {
-            "uuid": "",
-            "username": "",
-            "password": "",
-            "salt": "",
-            "md5": "",
-            "sha1": "",
-            "sha256": ""
-          },
-          "dob": {
-            "date": "",
-            "age": null
-          },
-          "registered": {
-            "date": "",
-            "age": null
-          },
-          "phone": "",
-          "cell": "",
-          "id": {
-            "name": "",
-            "value": ""
-          },
-          "picture": {
-            "large": "",
-            "medium": "",
-            "thumbnail": ""
-          },
-          "nat": ""
-    }
+    patients:[]
   },
-
   methods: {
     fetchPatients() {
-      fetch('https://randomuser.me/api')
+      fetch('dummy.php')
       .then(response => response.json())
-      .then(json => waitingApp.patients = json.results[0]);
+      .then(json => patientWaitingApp.patients = json);
 
     }
   },
